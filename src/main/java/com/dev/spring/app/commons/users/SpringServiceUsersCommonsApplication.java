3f1381b04f37a1011b384bf55298207077983872
Erick Estrada <erickestradapj@gmail.com>
2021-11-08 15:18:13 -0500

    modify

--- src/main/java/com/dev/spring/app/commons/users/SpringServiceUsersCommonsApplication.java
+++ src/main/java/com/dev/spring/app/commons/users/SpringServiceUsersCommonsApplication.java
@@ -1,6 +1,7 @@
 package com.dev.spring.app.commons.users;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class SpringServiceUsersCommonsApplication {
